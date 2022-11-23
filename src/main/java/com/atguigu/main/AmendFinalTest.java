722fcef1eabd90362b6a867dc2763569a0ae32da
liyuang <dicyuang@163.com>
2022-11-24 01:11:12 +0800

    td-commit-d

---
+++ src/main/java/com/atguigu/main/AmendFinalTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.main;
+
+public class AmendFinalTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("xxx");
+        System.out.println("i can believe it");
+        System.out.println("do you love me");
+    }
+}
