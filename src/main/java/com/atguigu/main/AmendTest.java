8ad5e041ecc89d6713e845d067c2a9c29cb92818
liyuang <dicyuang@163.com>
2022-11-24 01:05:37 +0800

    td-commit-c

--- src/main/java/com/atguigu/main/AmendTest.java
+++ src/main/java/com/atguigu/main/AmendTest.java
@@ -4,5 +4,7 @@ public class AmendTest{
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
+        System.out.println("amend-xxxx");
     }
 }
